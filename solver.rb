6ecef33f0e74e6fb3c192e8c539113f308239871
Jake Auswick <contact@mumbodesign.com>
2011-12-03 22:13:48 +0000

    example solver

--- solver.rb
+++ solver.rb
@@ -1,6 +1,8 @@
-$LOAD_PATH << './lib'
 require 'textcaptcha'
 
-
 solver = TextCaptcha.new
-puts solver.solve("Enter the number sixteen thousand two hundred and six in digits:")
+puts solver.solve("Of the numbers one hundred, eighty one, sixty one, 99, 51 or forty seven, which is the smallest?")
+puts solver.solve("If a person is called Robert, what is their name?")
+puts solver.solve("Pink, green, yellow, ant and finger: the 2nd colour is?")
+puts solver.solve("T-shirt, elbow, leg, milk and mosquito: how many body parts in the list?")
+puts solver.solve("What is six thousand one hundred and eighty seven as digits?")
